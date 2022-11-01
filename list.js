bc8fc87e7cc97264c3316a3de37eb50357093fe0
ReedWarburton <reedw1027@gmail.com>
2022-11-01 03:28:11 -0400

    Goodnight

---
+++ list.js
@@ -0,0 +1,12 @@
+lists = [
+    {
+      "ListNo": "51",
+      "items": ["cat", "dog", "mouse", "rat", "giraffe", "lion","zebra", "rhino", "ostrich"]
+  
+    },
+    {
+      "ListNo": "4234",
+      "items": ["donkey", "dog", "fish"]
+  
+    }
+  ]
---
+++ test.js
@@ -0,0 +1,12 @@
+list=[{
+    "Subject": [51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51],
+    "Word": ["cat", "dog", "mouse", "rat", "giraffe", "lion", "zebra", "rhino", "ostrich", "meerkat", "tiger", "bear", "bat", "aardvark", "elephant", "flea", "ferret", "parrot", "hippopotamus", "alligator", "crocodile", "frog", "whale", "shark", "clownfish", "guppy", "goldfish", "snake"]
+  }, 
+  {
+    "Subject": ["10", "10", "10", "10", "10", "10", "10"],
+    "Word": ["snail", "deer", "elk", "buffalo", "monkey", "hyena", "kangaroo"]
+  }, 
+  {
+    "Subject": [78, 78, 78, 78, 78, 78, 78, 78, 78, 78, 78],
+    "Word": ["orange", "apple", "banana", "starfruit", "clementine", "necterine", "tangerine", "mango", "grape", "blueberry", "raspberry"]
+  }] 
